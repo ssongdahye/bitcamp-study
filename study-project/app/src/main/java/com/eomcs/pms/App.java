096492293021b7f9aa230798acea364629b22694
ssongdahye <songdh121@gmail.com>
2021-07-12 19:50:01 +0900

    ,

--- study-project/app/src/main/java/com/eomcs/pms/App.java
+++ study-project/app/src/main/java/com/eomcs/pms/App.java
@@ -19,62 +19,160 @@ public class App {
     // 최대 입력 개수
     final int LENGTH = 100;
 
-    int[] no = new int[LENGTH];
-    String[] name = new String[LENGTH];
-    String[] email = new String[LENGTH];
-    String[] password = new String[LENGTH];
-    String[] photo = new String[LENGTH];
-    String[] tel = new String[LENGTH];
-    Date[] registeredDate = new Date[LENGTH];
+    int[] memberNo = new int[LENGTH];
+    String[] memberName = new String[LENGTH];
+    String[] memberEmail = new String[LENGTH];
+    String[] memberPassword = new String[LENGTH];
+    String[] memberPhoto = new String[LENGTH];
+    String[] memberTel = new String[LENGTH];
+    Date[] memberRegisteredDate = new Date[LENGTH];
 
+    int[] projectNo = new int[LENGTH];
+    String[] projectTitle = new String[LENGTH];
+    String[] projectContent = new String[LENGTH];
+    Date[] projectStartDate = new Date[LENGTH];
+    Date[] projectEndDate = new Date[LENGTH];
+    String[] projectOwner = new String[LENGTH];
+    String[] projectMembers = new String[LENGTH];
+
+    int[] no = new int[LENGTH];
+    String[] content = new String[LENGTH];
+    Date[] deadline = new Date[LENGTH];
+    String[] owner = new String[LENGTH];
+    int[] status = new int[LENGTH];
+
+    int memberSize = 0;
+    int projectSize = 0;
     int size = 0;
 
-    while (true) {
+    while(true) {
       System.out.print("명령> ");
       String input = keyboardScan.nextLine();
 
-      if(input.equals("exit") || input.equals("quit")) {
+      if (input.equals("exit") || input.equals("quit")) {
         System.out.println("안녕!");
         break;
-      } else if (input.equals("/member/add")) {
-        System.out.println("[회원등록]");
+      } else if(input.equals("/member/add")) {
+        System.out.println("[회원 등록]");
+
+        System.out.print("번호? ");
+        memberNo[memberSize] = Integer.parseInt(keyboardScan.nextLine());
+
+        System.out.print("이름? ");
+        memberName[memberSize] = keyboardScan.nextLine();
+
+        System.out.print("이메일? ");
+        memberEmail[memberSize] = keyboardScan.nextLine();
+
+        System.out.print("암호? ");
+        memberPassword[memberSize] = keyboardScan.nextLine();
+
+        System.out.print("사진? ");
+        memberPhoto[memberSize] = keyboardScan.nextLine();
+
+        System.out.print("전화? ");
+        memberTel[memberSize] = keyboardScan.nextLine();
+
+        memberRegisteredDate[memberSize] = new java.sql.Date(System.currentTimeMillis());
+
+        memberSize++;
+
+        System.out.println();
+      } else if(input.equals("/member/list")) {
+        System.out.println("[회원 목록]");
+
+        for (int i = 0; i < memberSize; i++) {
+          // 번호, 이름, 이메일, 전화, 가입일
+          System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
+              memberNo[i], memberName[i], memberEmail[i], memberTel[i], memberRegisteredDate[i]);
+        }
+
+        System.out.println();
+      } else if(input.equals("/project/add")) {
+        System.out.println("[프로젝트 등록]");
+        System.out.print("번호? ");
+        projectNo[projectSize] = Integer.valueOf(keyboardScan.nextLine());
+
+        System.out.print("프로젝트명? ");
+        projectTitle[projectSize] = keyboardScan.nextLine();
+
+        System.out.print("내용? ");
+        projectContent[projectSize] = keyboardScan.nextLine();
+
+        System.out.print("시작일? ");
+        projectStartDate[projectSize] = Date.valueOf(keyboardScan.nextLine());
+
+        System.out.print("종료일? ");
+        projectEndDate[projectSize] = Date.valueOf(keyboardScan.nextLine());
+
+        System.out.print("만든이? ");
+        projectOwner[projectSize] = keyboardScan.nextLine();
+
+        System.out.print("팀원? ");
+        projectMembers[projectSize] = keyboardScan.nextLine();
+
+        projectSize++;
+
+        System.out.println();
+      } else if(input.equals("/project/list")) {
+        System.out.println("[프로젝트 목록]");
+
+        for (int i = 0; i < projectSize; i++) {
+          // 번호, 프로젝트명, 시작일, 종료일, 만든이
+          System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
+              projectNo[i], projectTitle[i], projectStartDate[i], projectEndDate[i], projectOwner[i]);
+        }
+
+        System.out.println();
+      } else if(input.equals("/task/add")) {
+        System.out.println("[작업 등록]");
 
         System.out.print("번호? ");
         no[size] = Integer.parseInt(keyboardScan.nextLine());
 
-        System.out.print("이름? ");
-        name[size] = keyboardScan.nextLine();
+        System.out.print("내용? ");
+        content[size] = keyboardScan.nextLine();
 
-        System.out.print("이메일? ");
-        email[size] = keyboardScan.nextLine();
+        System.out.print("마감일? ");
+        deadline[size] = Date.valueOf(keyboardScan.nextLine());
 
-        System.out.print("암호? ");
-        password[size] = keyboardScan.nextLine();
+        System.out.println("상태?");
+        System.out.println("0: 신규");
+        System.out.println("1: 진행중");
+        System.out.println("2: 완료");
+        System.out.print("> ");
+        status[size] = Integer.valueOf(keyboardScan.nextLine());
 
-        System.out.print("사진? ");
-        photo[size] = keyboardScan.nextLine();
-
-        System.out.print("전화? ");
-        tel[size] = keyboardScan.nextLine();
-
-        registeredDate[size] = new Date(System.currentTimeMillis());
+        System.out.print("담당자? ");
+        owner[size] = keyboardScan.nextLine();
 
         size++;
-
-      } else if (input.equals("/member/list")) {
-        System.out.println("[회원목록]");
+        System.out.println(); // 빈 줄 출력
+      } else if(input.equals("/task/list")) {
+        System.out.println("[작업 목록]");
 
         for (int i = 0; i < size; i++) {
-          // 번호, 이름, 이메일, 전화, 가입일
+          String stateLabel = null;
+          switch (status[i]) {
+            case 1:
+              stateLabel = "진행중";
+              break;
+            case 2:
+              stateLabel = "완료";
+              break;
+            default:
+              stateLabel = "신규";
+          }
+          // 번호, 작업명, 마감일, 프로젝트, 상태, 담당자
           System.out.printf("%d, %s, %s, %s, %s\n", // 출력 형식 지정
-              no[i], name[i], email[i], tel[i], registeredDate[i]);
+              no[i], content[i], deadline[i], stateLabel, owner[i]);
         }
+
+        System.out.println();
       } else {
         System.out.println("실행할 수 없는 명령입니다.");
-      }
-      System.out.println();
+      } 
     }
-
     keyboardScan.close();
   }
 }
