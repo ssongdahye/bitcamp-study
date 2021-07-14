fd64fcce802d268fbca715c51be0fbcc3d4b20a4
ssongdahye <songdh121@gmail.com>
2021-07-14 17:49:19 +0900

    ,

--- java-basic/src/main/java/com/eomcs/lang/ex07/Exam0110.java
+++ java-basic/src/main/java/com/eomcs/lang/ex07/Exam0110.java
@@ -2,35 +2,43 @@ package com.eomcs.lang.ex07;
 
 import java.util.Scanner;
 
-//# 메서드 : 사용 전
-//
+//1단계: 공백 출력 코드를 메서드로 추출하기 printSpaces
+//2단계: 별을 출력하는 코드를 메서드로 추출하기 printStars
+//3단계: while 대신 for 문 사용하기
+//4단계: 공백 계산 식을 메서드로 추출하기 getSpaceLength
 public class Exam0110 {
+
+  static void printSpaces(int len) {
+    for (int i=0; i<len; i++) {
+      System.out.print(" ");
+    }    
+  }
+
+  static void printStars(int len) {
+    for (int starCnt=0; starCnt<len; starCnt++) {
+      System.out.print("*");
+    }    
+  }
+
+  static int getSpaceLength(int totalStar, int displayStar) {
+    return (totalStar-displayStar)/2;
+  }
+
   public static void main(String[] args) {
     Scanner keyScan = new Scanner(System.in);
     System.out.print("밑변의 길이? ");
     int len = keyScan.nextInt();
     keyScan.close();
 
-    int starLen = 1;
-    while (starLen <= len) {
+
+    for (int starLen = 1; starLen <= len; starLen += 2) {
       // 별 앞에 공백 출력
-      int spaceCnt = 1;
-      int spaceLen = (len - starLen) / 2;
-      while (spaceCnt <= spaceLen) {
-        System.out.print(" ");
-        spaceCnt++;
-      }
-
+      printSpaces(getSpaceLength(len, starLen));
       // 별 출력
-      int starCnt = 1;
-      while (starCnt <= starLen) {
-        System.out.print("*");
-        starCnt++;
-      }
-
+      printStars(starLen);
       // 출력 줄 바꾸기
       System.out.println();
-      starLen += 2;
+
     }
   }
 }
