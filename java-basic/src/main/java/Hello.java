207e4a0661590b9a53c34c56e46c2e01f4c7ceca
ssongdahye <songdh121@gmail.com>
2021-07-20 18:50:59 +0900

    ,

--- java-basic/src/main/java/Hello.java
+++ java-basic/src/main/java/Hello.java
@@ -1,11 +1,23 @@
 class Hello {
-    // --------------------------------------
-    // | main()                             |
-    // |- 프로그램 실행 시작점              |
-    // |- 'entry point'라 부름              |
-    // --------------------------------------
-    public static void main(String[] args){
-        System.out.println("Hello, world!");
-        System.out.println("안녕하세요!");
-    }
+  // --------------------------------------
+  // | main()                             |
+  // |- 프로그램 실행 시작점              |
+  // |- 'entry point'라 부름              |
+  // --------------------------------------
+  public static void main(String[] args){
+    System.out.println("Hello, world!");
+    System.out.println("안녕하세요!");
+
+    int a = 20;
+    int b = ++a;
+
+    System.out.println(a);
+    System.out.println(b);
+
+    String s4 = "30";
+
+    int i = 20;
+
+    System.out.println(s4 + i);
+  }
 }
