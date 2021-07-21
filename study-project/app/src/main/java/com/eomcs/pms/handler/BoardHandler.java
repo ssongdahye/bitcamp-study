7aef406d3a8a2d5cc77540c57c40900ec3fd4be7
ssongdahye <songdh121@gmail.com>
2021-07-21 18:05:47 +0900

    ,

--- study-project/app/src/main/java/com/eomcs/pms/handler/BoardHandler.java
+++ study-project/app/src/main/java/com/eomcs/pms/handler/BoardHandler.java
@@ -15,10 +15,7 @@ public class BoardHandler {
   Board[] boards = new Board[MAX_LENGTH];
   int size = 0;
 
-  // BoardHandler 설계도에 따라 만든 변수(boards, size)를 다룰 수 있도록
-  // 파라미터로 인스턴스 주소를 받는다.
-  // 
-  public static void add(BoardHandler that) {
+  public void add() {
     System.out.println("[새 게시글]");
 
     Board board = new Board();
@@ -31,20 +28,119 @@ public class BoardHandler {
 
     System.out.println("게시글을 등록하였습니다.");
 
-    that.boards[that.size++] = board;
+    this.boards[this.size++] = board;
   }
 
-  public static void list(BoardHandler that) {
+  public void list() {
     System.out.println("[게시글 목록]");
-    for (int i = 0; i < that.size; i++) {
+    for (int i = 0; i < this.size; i++) {
       System.out.printf("%d, %s, %s, %s, %d, %d\n", 
-          that.boards[i].no, 
-          that.boards[i].title, 
-          that.boards[i].registeredDate, 
-          that.boards[i].writer, 
-          that.boards[i].viewCount,
-          that.boards[i].like);
+          this.boards[i].no, 
+          this.boards[i].title, 
+          this.boards[i].registeredDate, 
+          this.boards[i].writer, 
+          this.boards[i].viewCount,
+          this.boards[i].like);
     }
+  }
+
+  public void detail() {
+    System.out.println("[게시글 상세보기]");
+
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = null;
+
+    for (int i = 0; i < this.size; i++) {
+      if(this.boards[i].no == no) {
+        board = this.boards[i];
+        break;
+      } 
+    }
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    System.out.println("제목: " + board.title);
+    System.out.println("내용: " + board.content);
+    System.out.println("작성자: " + board.writer);
+    System.out.println("등록일: " + board.registeredDate);
+    System.out.println("조회수: " + ++board.viewCount);
+  }
+
+  public void update() {
+    System.out.println("[게시글 변경]");
+
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = null;
+
+    for (int i = 0; i < this.size; i++) {
+      if(this.boards[i].no == no) {
+        board = this.boards[i];
+        break;
+      } 
+    }
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    String title = Prompt.inputString(String.format("제목(%s)? ", board.title));
+    String content = Prompt.inputString(String.format("내용(%s)? ", board.content));
+
+    String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+
+    if(input.equalsIgnoreCase("n") || input.length() == 0) {
+      System.out.println("게시글 변경을 취소하였습니다.");
+      return;
+    }
+
+    board.title = title;
+    board.content = content;
+    System.out.println("게시글을 변경하였습니다.");
+
+
+    //String title = Prompt.inputString("제목(" + board.title + ")? ");
+    //String content = Prompt.inputString("내용(" + board.content + ")? ");
+
+    /*
+     * if(Prompt.inputString("정말 변경하시겠습니까?(y/N) ").equals("y")) { board.title = title; board.content
+     * = content;
+     * 
+     * System.out.println("게시글을 변경하였습니다."); } else { System.out.println("게시글 변경을 취소하였습니다."); }
+     */
+  }
+
+  public void delete() {
+    System.out.println("[게시글 삭제]");
+
+    int no = Prompt.inputInt("번호? ");
+
+    Board board = null;
+
+    for (int i = 0; i < this.size; i++) {
+      if(this.boards[i].no == no) {
+        board = this.boards[i];
+        break;
+      } 
+    }
+
+    if (board == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    if(Prompt.inputString("정말 삭제하시겠습니까?(y/N) ").equals("y")) {
+
+      System.out.println("게시글을 삭제하였습니다.");
+    } else {
+      System.out.println("게시글 삭제를 취소하였습니다.");
+    }
+
   }
 
 }
