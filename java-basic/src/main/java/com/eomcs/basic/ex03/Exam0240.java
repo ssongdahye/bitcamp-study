6ec080533bc3f8fda1629fb70b5932ef4dec66cd
ssongdahye <songdh121@gmail.com>
2021-08-24 15:57:05 +0900

    ,

--- java-basic/src/main/java/com/eomcs/basic/ex03/Exam0240.java
+++ java-basic/src/main/java/com/eomcs/basic/ex03/Exam0240.java
@@ -2,6 +2,7 @@
 package com.eomcs.basic.ex03;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.function.Consumer;
 
 public class Exam0240 {
@@ -59,6 +60,55 @@ public class Exam0240 {
     list.add(m2);
     list.add(m3);
 
+    // ArrayList에서 목록 뽑아내는 방법
+    // 1) Exam0210
+    System.out.println("-------------1---------------");
+    for (int i = 0; i < list.size(); i++) {
+      Member m = list.get(i);
+      System.out.printf("%s(%d)\n", m.name, m.age);
+    }
+
+    // 2) Exam0220
+    System.out.println("-------------2---------------");
+    Object[] arr = list.toArray();
+    for (Object obj : arr) {
+      Member m = (Member) obj;
+      System.out.printf("%s(%d)\n", m.name, m.age);
+    }
+
+    // 3) Exam0221
+    System.out.println("-------------3---------------");
+    Member[] arr1 = list.toArray(new Member[list.size()]); // 파라미터로 받은 배열 그대로 리턴
+    // Member[] arr1 = list.toArray(new Member[0]); // 새로 배열을 만들어 리턴
+    for (Member m : arr1) {
+      System.out.printf("%s(%d)\n", m.name, m.age);
+    }
+
+    // 4) Exam0230
+    System.out.println("-------------4---------------");
+    Iterator<Member> iterator = list.iterator();
+    while (iterator.hasNext()) {
+      Member m = iterator.next();
+      System.out.printf("%s(%d)\n", m.name, m.age);
+    }
+
+    // 5-1)
+    System.out.println("-------------5.1---------------");
+    Consumer<Member> consumer = new Consumer<Member>() {
+      public void accept(Member m) {
+        System.out.printf("%s(%d)\n", m.name, m.age);
+      };
+    };
+    list.forEach(consumer);
+
+    // 5-2) 
+    System.out.println("-------------5.2---------------");
+    list.forEach(m -> System.out.printf("%s(%d)\n", m.name, m.age));
+
+    // 6) Exam0244 가서 보기
+    // 메서드 레퍼런스로 인터페이스 구현체 전달하기
+
+
     // forEach() 메서드에게 넘길 객체
     // => Consumer 규칙에 따라 만들어야 한다.
     // => List 보관된 객체를 반복문을 통해 꺼낼 때 마다
@@ -80,8 +130,9 @@ public class Exam0240 {
     //    지금 내가 파라미터로 넘겨주는 객체 있지?
     //    MyConsumer 객체 말이야.
     //    이 객체의 accept()를 호출해주렴.
-    list.forEach(new MyConsumer());
+    //list.forEach(new MyConsumer());
   }
+
 }
 
 
