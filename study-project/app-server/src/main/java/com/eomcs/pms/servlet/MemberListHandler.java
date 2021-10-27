0f48f2a014c80c869b5737c103dc566229260536
ssongdahye <songdh121@gmail.com>
2021-10-27 16:58:30 +0900

    ,

--- study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberListHandler.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberListHandler.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Member;
 
+
 @WebServlet("/member/list")
 public class MemberListHandler extends GenericServlet {
   private static final long serialVersionUID = 1L;
@@ -28,24 +29,40 @@ public class MemberListHandler extends GenericServlet {
   @Override
   public void service(ServletRequest request, ServletResponse response)
       throws ServletException, IOException {
-    // 맨 처음에 init 호출 되고 나서 부턴 계속 service가 호출된다
 
     response.setContentType("text/html;charset=UTF-8");
     PrintWriter out = response.getWriter();
 
+    out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("<head>");
     out.println("  <title>회원목록</title>");
     out.println("</head>");
     out.println("<body>");
-    out.println("<h1>[회원 목록]</h1>");
-    out.println("<a href='form'>새회원</a><br>");
-
+    out.println("<h1>회원 목록</h1>");
+    out.println("<div><a href='form'>새회원</a></div>");
+    out.println("<table border='1'>");
+    out.println("<thead>");
+    out.println("  <tr>");
+    out.println("    <th>번호</th>");
+    out.println("    <th>이름</th>");
+    out.println("    <th>이메일</th>");
+    out.println("    <th>전화</th>");
+    out.println("    <th>등록일</th>");
+    out.println("  <tr>");
+    out.println("</thread>");
+    out.println("<tbody>");
     try {
       Collection<Member> memberList = memberDao.findAll();
 
       for (Member member : memberList) {
-        out.printf("%d, <a href='detail?no=%1$d'>%s</a>, %s, %s, %s<br>", 
+        out.printf("<tr>"
+            + "<td>%d</td>"
+            + " <td><a href='detail?no=%1$d'>%s</a></td>"
+            + " <td>%s</td>"
+            + " <td>%s</td>"
+            + " <td>%s</td>"
+            + "</tr>", 
             member.getNo(), 
             member.getName(), 
             member.getEmail(), 
@@ -55,6 +72,8 @@ public class MemberListHandler extends GenericServlet {
     } catch (Exception e) {
       throw new ServletException(e);
     }
+    out.println("</tbody>");
+    out.println("</table>");
     out.println("</body>");
     out.println("</html>");
   }
