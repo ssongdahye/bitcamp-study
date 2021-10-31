61817bf7b8bc2345526fde8df2520620cf5f37e8
ssongdahye <songdh121@gmail.com>
2021-11-01 01:32:09 +0900

    ,

--- study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberAddController.java
+++ study-project/app-server/src/main/java/com/eomcs/pms/servlet/MemberAddController.java
@@ -1,7 +1,7 @@
 package com.eomcs.pms.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -13,8 +13,9 @@ import org.apache.ibatis.session.SqlSession;
 import com.eomcs.pms.dao.MemberDao;
 import com.eomcs.pms.domain.Member;
 
+
 @WebServlet("/member/add")
-public class MemberAddHandler extends HttpServlet {
+public class MemberAddController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   SqlSession sqlSession;
@@ -23,23 +24,13 @@ public class MemberAddHandler extends HttpServlet {
   @Override
   public void init(ServletConfig config) throws ServletException {
     ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
     sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
+    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
   }
 
   @Override
   protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<html>");
-    out.println("<head>");
-    out.println("  <title>회원등록</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1>회원등록결과</h1>");
 
     Member member = new Member();
 
@@ -52,25 +43,18 @@ public class MemberAddHandler extends HttpServlet {
     try {
       memberDao.insert(member);
       sqlSession.commit();
-
-      out.println("회원을 등록했습니다.");
-
-      out.println("<a href='list'>[목록]</a>");
+      response.setHeader("Refresh", "1;url=list");
+      request.getRequestDispatcher("MemberAdd.jsp").forward(request, response);
 
     } catch (Exception e) {
-      out.println("목록 조회 오류!");
-      e.printStackTrace();
+      // 오류를 출력할 때 사용할 수 있도록 예외 객체를 저장소에 보관한다.
+      request.setAttribute("error", e);
+
+      // 오류가 발생하면, 오류 내용을 출력할 뷰를 호출한다.
+      RequestDispatcher 요청배달자 = request.getRequestDispatcher("/Error.jsp");
+      요청배달자.forward(request, response);
     }
-
-    out.println("</body>");
-    out.println("</html>");
-
-    // 리프래시(refresh)
-    // 웹브라우저에게 서버가 보내준 HTML을 출력한 후
-    // 1초가 경과하면 지정한 URL을 다시 요청하도록 명령한다.
-    response.setHeader("Refresh", "1;url=list");
   }
-
 }
 
 
--- study-project/app-server/src/main/webapp/member/MemberForm.jsp
+++ study-project/app-server/src/main/webapp/member/MemberForm.jsp
@@ -5,34 +5,27 @@
 <html>
 <head>
   <title>새회원</title>
-<style>
-	label {
-	  display: inline-block;
-	  text-align: right;
-	  margin-right: 5px;
-	  width: 60px;
-	}
-</style>
+  <style>
+  label {
+    margin-right: 5px;
+    text-align: right;
+    display: inline-block;
+    width: 60px;
+  }
+  </style>
 </head>
 <body>
-<h1>새회원</h1>
-	<form action='MemberAdd.jsp'>
-	<label for='f-name'>이름</label> 
-	<input id='f-name' type='text' name='name'><br>
-	<label for='f-email'>이메일</label> 
-	<input id='f-email' type='email' name='email'><br>
-	<label for='f-password'>암호</label>
-	<input id='f-password' type='password' name='password'><br>
-	<label for='f-photo'>사진</label> 
-	<input id='f-photo' type='text' name='photo'><br>
-	<label for='f-tel'>전화</label> 
-	<input id='f-tel' type='tel' name='tel'><br>
-	<button>등록</button><br>
+<h1>새회원(MVC)</h1>
+<form action='add'>
+<label for='f-name'>이름</label> <input id='f-name' type='text' name='name'><br>
+<label for='f-email'>이메일</label> <input id='f-email' type='email' name='email'><br>
+<label for='f-password'>암호</label> <input id='f-password' type='password' name='password'><br>
+<label for='f-photo'>사진</label> <input id='f-photo' type='text' name='photo'><br>
+<label for='f-tel'>전화</label> <input id='f-tel' type='tel' name='tel'><br>
+<button>등록</button><br>
 </form>
 </body>
 </html>
-
-
 
 
 
